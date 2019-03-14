4e93d7ccc8e74a7171f4864923f4d686d0f0f677
preciousidehen <48460830+preciousidehen@users.noreply.github.com>
2019-03-14 14:37:27 -0500

    Add files via upload

---
+++ factorial_of_number.cpp
@@ -0,0 +1,29 @@
+/*
+precious idehen
+
+program : this program finds the factorial of a number by asking the
+		  user to enter a number. it's validates the users input if 
+		  it is less than zero.
+*/
+
+#include <iostream>
+using namespace std;
+
+int factorial(int num) {
+	if(num != 1) {
+		num--;
+		return num * factorial(num);
+	}
+}
+int main() {
+	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	while(num < 0) {
+		cout<<"invalid input"<<endl;
+		cout<<"Enter a valid input: ";
+		cin>>num;
+	}
+	cout<<"The factorial of "<<num<<" is "<<factorial(num + 1);
+}
